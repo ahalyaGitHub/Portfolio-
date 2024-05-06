1bb5ec2e477d8c38f82e2b048bcaee6f611c061c
ahalya_r <127035624+Ahalyagithub@users.noreply.github.com>
2024-05-07 00:59:28 +0530

    Added section and footer

---
+++ main.js
@@ -0,0 +1,7 @@
+const education = () => {
+    document.getElementsByTagName('section')[0].scrollIntoView({ behavior: 'smooth' });
+}
+
+const contactMe = () => {
+    document.getElementsByTagName('footer')[0].scrollIntoView({ behavior: 'smooth' });
+}
